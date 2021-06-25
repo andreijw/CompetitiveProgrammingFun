030b09a003385581f4a8c597d1221af21a3880d5
Andrei Jaume <andrei.jaumewillenska@utexas.edu>
2021-06-25 17:50:37 -0500

    finished up binary / rgb modes

--- ImageToText/imageToTextParser.py
+++ ImageToText/imageToTextParser.py
@@ -25,16 +25,19 @@ def read_image(image_path):
 def gray_scale_image(image):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray_image = cv2.GaussianBlur(gray_image, (3,3), 0)
-    #gray_image = cv2.GaussianBlur(gray_image, (7,7), 0)
     return gray_image
 
-def threshold_image(image):
-    #t_image = cv2.adaptiveThreshold(image, 250, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 7, 2)
-    (t, t_image) = cv2.threshold(image, 5, 255, cv2.THRESH_BINARY_INV)
-    # (t, t_image) = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+def threshold_image(image, mode):
+    if mode == "binary":
+        (t, t_image) = cv2.threshold(image, 135, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+    else:
+        (t, t_image) = cv2.threshold(image, 50, 255, cv2.THRESH_BINARY_INV)
     return t_image
 
-def morph_opening(image):
+def morph_opening(image, mode):
+    if mode == "binary":
+        return image
+    
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
     o_image = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel, iterations=1)
     return o_image
@@ -45,13 +48,11 @@ def process_image(image):
     contents = pytesseract.image_to_string(image, lang='eng', config=configuration)
     return contents
     
-def apply_mask(original_image, binary_image):
-    #inverted_image = cv2.bitwise_not(binary_image)
-    m_image = cv2.bitwise_and(original_image, original_image, mask=binary_image)
-    return m_image
-    
-def find_contours(image):
-    new_image = image
+def find_contours(image, mode):
+    if mode == "binary":
+        return image
+        
+    new_image = np.copy(image)
     contours, hierarchy = cv2.findContours(new_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)  
     print("found the following number of contours", len(contours))
     
@@ -60,22 +61,31 @@ def find_contours(image):
     
 def main():
     # User will input the image path
-    if len(sys.argv) < 2:
-        print("Please enter an image path")
+    if len(sys.argv) < 3:
+        print("Please enter an image path and mode of operation")
         return
     
     # read the image into an array of pixels
-    image_path = sys.argv[1]
+    mode = sys.argv[1]
+    image_path = sys.argv[2]
     image = read_image(image_path)
     
     pytesseract.pytesseract.tesseract_cmd = r'D:\\Program Files (x86)\\Tesseract\\tesseract.exe'
-
+        
     # preprocess the image
+    if mode == "binary":
+        print("image is already in binary colors")
+    else:
+        print("image is in rgb colors")
+
     gray_image = gray_scale_image(image)
-    t_image = threshold_image(gray_image)  
-    c_image = find_contours(t_image)
-    o_image = morph_opening(c_image)
-    #m_image = apply_mask(gray_image, c_image)
+    # (unique, counts) = np.unique(gray_image, return_counts=True)
+    # frequencies = np.asarray((unique, counts)).T
+    # print(frequencies)
+    
+    t_image = threshold_image(gray_image, mode)
+    c_image = find_contours(t_image, mode)
+    o_image = morph_opening(c_image, mode)
     
     # display the images
     cv2.imshow('Original Image', image)
